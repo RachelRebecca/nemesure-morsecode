363cfa2ba62dc17ce4024cf5462478685aee9b41
RachelRebecca <RachelRebecca67@gmail.com>
2022-03-24 23:19:34 -0400

    added mockito dependency

--- src/test/java/MorseCodePresenterTest.java
+++ src/test/java/MorseCodePresenterTest.java
@@ -1,6 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class MorseCodePresenterTest
 {
+    @Test
+    public void translateToEnglish()
+    {
 
+    }
+
+    @Test
+    public void translateToMorse()
+    {
+
+    }
 }
