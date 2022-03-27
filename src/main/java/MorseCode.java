16775ff872d15f44651806ef3a6a44ed4e688645
RachelRebecca <RachelRebecca67@gmail.com>
2022-03-27 18:53:34 -0400

    renamed hashmaps

--- src/main/java/MorseCode.java
+++ src/main/java/MorseCode.java
@@ -2,121 +2,121 @@ import java.util.HashMap;
 
 public class MorseCode
 {
-    private final HashMap<Character, String> morseCharacters;
-    private final HashMap<String, Character> morseCharactersOtherDirection;
+    private final HashMap<Character, String> englishToMorse;
+    private final HashMap<String, Character> morseToEnglish;
 
     public MorseCode()
     {
-        morseCharacters = new HashMap<>();
+        englishToMorse = new HashMap<>();
         addMorseCode();
-        morseCharactersOtherDirection = new HashMap<>();
+        morseToEnglish = new HashMap<>();
         addOtherDirection();
     }
 
     private void addMorseCode()
     {
-        morseCharacters.put('A', ".-");
-        morseCharacters.put('B', "-...");
-        morseCharacters.put('C', "-.-.");
-        morseCharacters.put('D', "-..");
-        morseCharacters.put('E', ".");
-        morseCharacters.put('F', "..-.");
-        morseCharacters.put('G', "--.");
-        morseCharacters.put('H', "....");
-        morseCharacters.put('I', "..");
-        morseCharacters.put('J', ".---");
-        morseCharacters.put('K', "-.-");
-        morseCharacters.put('L', ".-..");
-        morseCharacters.put('M', "--");
-        morseCharacters.put('N', "-.");
-        morseCharacters.put('O', "---");
-        morseCharacters.put('P', ".--.");
-        morseCharacters.put('Q', "--.-");
-        morseCharacters.put('R', ".-.");
-        morseCharacters.put('S', "...");
-        morseCharacters.put('T', "-");
-        morseCharacters.put('U', "..-");
-        morseCharacters.put('V', "...-");
-        morseCharacters.put('W', ".--");
-        morseCharacters.put('X', "-..-");
-        morseCharacters.put('Y', "-.--");
-        morseCharacters.put('Z', "--..");
-        morseCharacters.put('0', "-----");
-        morseCharacters.put('1', ".----");
-        morseCharacters.put('2', "..---");
-        morseCharacters.put('3', "...--");
-        morseCharacters.put('4', "....-");
-        morseCharacters.put('5', ".....");
-        morseCharacters.put('6', "-....");
-        morseCharacters.put('7', "--...");
-        morseCharacters.put('8', "---..");
-        morseCharacters.put('9', "----.");
-        morseCharacters.put('.', ".-.-.-");
-        morseCharacters.put(',', "--..--");
-        morseCharacters.put(':', "---...");
-        morseCharacters.put('?', "..--..");
-        morseCharacters.put('\'', ".----.");
-        morseCharacters.put('-', "-....-");
-        morseCharacters.put('/', "-..-.");
-        morseCharacters.put('"', ".-..-.");
+        englishToMorse.put('A', ".-");
+        englishToMorse.put('B', "-...");
+        englishToMorse.put('C', "-.-.");
+        englishToMorse.put('D', "-..");
+        englishToMorse.put('E', ".");
+        englishToMorse.put('F', "..-.");
+        englishToMorse.put('G', "--.");
+        englishToMorse.put('H', "....");
+        englishToMorse.put('I', "..");
+        englishToMorse.put('J', ".---");
+        englishToMorse.put('K', "-.-");
+        englishToMorse.put('L', ".-..");
+        englishToMorse.put('M', "--");
+        englishToMorse.put('N', "-.");
+        englishToMorse.put('O', "---");
+        englishToMorse.put('P', ".--.");
+        englishToMorse.put('Q', "--.-");
+        englishToMorse.put('R', ".-.");
+        englishToMorse.put('S', "...");
+        englishToMorse.put('T', "-");
+        englishToMorse.put('U', "..-");
+        englishToMorse.put('V', "...-");
+        englishToMorse.put('W', ".--");
+        englishToMorse.put('X', "-..-");
+        englishToMorse.put('Y', "-.--");
+        englishToMorse.put('Z', "--..");
+        englishToMorse.put('0', "-----");
+        englishToMorse.put('1', ".----");
+        englishToMorse.put('2', "..---");
+        englishToMorse.put('3', "...--");
+        englishToMorse.put('4', "....-");
+        englishToMorse.put('5', ".....");
+        englishToMorse.put('6', "-....");
+        englishToMorse.put('7', "--...");
+        englishToMorse.put('8', "---..");
+        englishToMorse.put('9', "----.");
+        englishToMorse.put('.', ".-.-.-");
+        englishToMorse.put(',', "--..--");
+        englishToMorse.put(':', "---...");
+        englishToMorse.put('?', "..--..");
+        englishToMorse.put('\'', ".----.");
+        englishToMorse.put('-', "-....-");
+        englishToMorse.put('/', "-..-.");
+        englishToMorse.put('"', ".-..-.");
     }
 
     public void addOtherDirection()
     {
-        morseCharactersOtherDirection.put(".-", 'A');
-        morseCharactersOtherDirection.put("-...", 'B');
-        morseCharactersOtherDirection.put("-.-.", 'C');
-        morseCharactersOtherDirection.put("-..", 'D');
-        morseCharactersOtherDirection.put(".", 'E');
-        morseCharactersOtherDirection.put("..-.", 'F');
-        morseCharactersOtherDirection.put("--.", 'G');
-        morseCharactersOtherDirection.put("....", 'H');
-        morseCharactersOtherDirection.put("..", 'I');
-        morseCharactersOtherDirection.put(".---", 'J');
-        morseCharactersOtherDirection.put("-.-", 'K');
-        morseCharactersOtherDirection.put(".-..", 'L');
-        morseCharactersOtherDirection.put("--", 'M');
-        morseCharactersOtherDirection.put("-.", 'N');
-        morseCharactersOtherDirection.put("---", 'O');
-        morseCharactersOtherDirection.put(".--.", 'P');
-        morseCharactersOtherDirection.put("--.-", 'Q');
-        morseCharactersOtherDirection.put(".-.", 'R');
-        morseCharactersOtherDirection.put("...", 'S');
-        morseCharactersOtherDirection.put("-", 'T');
-        morseCharactersOtherDirection.put("..-", 'U');
-        morseCharactersOtherDirection.put("...-", 'V');
-        morseCharactersOtherDirection.put(".--", 'W');
-        morseCharactersOtherDirection.put("-..-", 'X');
-        morseCharactersOtherDirection.put("-.--", 'Y');
-        morseCharactersOtherDirection.put("--..", 'Z');
-        morseCharactersOtherDirection.put("-----", '0');
-        morseCharactersOtherDirection.put(".----", '1');
-        morseCharactersOtherDirection.put("..---", '2');
-        morseCharactersOtherDirection.put("...--", '3');
-        morseCharactersOtherDirection.put("....-", '4');
-        morseCharactersOtherDirection.put(".....", '5');
-        morseCharactersOtherDirection.put("-....", '6');
-        morseCharactersOtherDirection.put("--...", '7');
-        morseCharactersOtherDirection.put("---..", '8');
-        morseCharactersOtherDirection.put("----.", '9');
-        morseCharactersOtherDirection.put(".-.-.-", '.');
-        morseCharactersOtherDirection.put("--..--", ',');
-        morseCharactersOtherDirection.put("---...", ':');
-        morseCharactersOtherDirection.put("..--..", '?');
-        morseCharactersOtherDirection.put(".----.", '\'');
-        morseCharactersOtherDirection.put("-....-", '-');
-        morseCharactersOtherDirection.put("-..-.", '/');
-        morseCharactersOtherDirection.put(".-..-.", '"');
+        morseToEnglish.put(".-", 'A');
+        morseToEnglish.put("-...", 'B');
+        morseToEnglish.put("-.-.", 'C');
+        morseToEnglish.put("-..", 'D');
+        morseToEnglish.put(".", 'E');
+        morseToEnglish.put("..-.", 'F');
+        morseToEnglish.put("--.", 'G');
+        morseToEnglish.put("....", 'H');
+        morseToEnglish.put("..", 'I');
+        morseToEnglish.put(".---", 'J');
+        morseToEnglish.put("-.-", 'K');
+        morseToEnglish.put(".-..", 'L');
+        morseToEnglish.put("--", 'M');
+        morseToEnglish.put("-.", 'N');
+        morseToEnglish.put("---", 'O');
+        morseToEnglish.put(".--.", 'P');
+        morseToEnglish.put("--.-", 'Q');
+        morseToEnglish.put(".-.", 'R');
+        morseToEnglish.put("...", 'S');
+        morseToEnglish.put("-", 'T');
+        morseToEnglish.put("..-", 'U');
+        morseToEnglish.put("...-", 'V');
+        morseToEnglish.put(".--", 'W');
+        morseToEnglish.put("-..-", 'X');
+        morseToEnglish.put("-.--", 'Y');
+        morseToEnglish.put("--..", 'Z');
+        morseToEnglish.put("-----", '0');
+        morseToEnglish.put(".----", '1');
+        morseToEnglish.put("..---", '2');
+        morseToEnglish.put("...--", '3');
+        morseToEnglish.put("....-", '4');
+        morseToEnglish.put(".....", '5');
+        morseToEnglish.put("-....", '6');
+        morseToEnglish.put("--...", '7');
+        morseToEnglish.put("---..", '8');
+        morseToEnglish.put("----.", '9');
+        morseToEnglish.put(".-.-.-", '.');
+        morseToEnglish.put("--..--", ',');
+        morseToEnglish.put("---...", ':');
+        morseToEnglish.put("..--..", '?');
+        morseToEnglish.put(".----.", '\'');
+        morseToEnglish.put("-....-", '-');
+        morseToEnglish.put("-..-.", '/');
+        morseToEnglish.put(".-..-.", '"');
     }
 
     public String translateFromEnglishToMorse(String letter)
     {
         Character charLetter = letter.charAt(0);
-        return morseCharacters.get(charLetter);
+        return englishToMorse.get(charLetter);
     }
 
     public Character translateFromMorseToEnglish(String morse)
     {
-        return morseCharactersOtherDirection.get(morse);
+        return morseToEnglish.get(morse);
     }
 }
